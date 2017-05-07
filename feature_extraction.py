fba245cfc3b37fe581d40467b6789b9c20880997
Bachmann, Michael <mbachm87@gmail.com>
2017-05-07 17:23:23 +0200

    Added heatmap function. Changed some feature extraction params

--- feature_extraction.py
+++ feature_extraction.py
@@ -15,6 +15,9 @@ hog_channel = 'ALL'
 
 def set_parameters(cspace, s_size, h_bins, h_range, orient, pix_cell,
 	cell_block, h_channel):
+	""" This functions set all needed parameters for the feature extraction.
+		Has to be done once, otherwise the default parameters will be used.
+	"""
 	global colorspace
 	colorspace = cspace
 	global spatial_size
@@ -53,26 +56,23 @@ def __convert_to_colorspace(img):
 		else: feature_image = np.copy(img)
 	return feature_image
 
-# Define a function to compute binned color features  
 def __bin_spatial(img):
+	""" Function to compute binned color features """
 	color1 = cv2.resize(img[:,:,0], spatial_size).ravel()
 	color2 = cv2.resize(img[:,:,1], spatial_size).ravel()
 	color3 = cv2.resize(img[:,:,2], spatial_size).ravel()
 	return np.hstack((color1, color2, color3))
 
-# Define a function to compute color histogram features  
 def __color_hist(img):
-	# Compute the histogram of the color channels separately
+	""" Function to compute color histogram features """
 	channel1_hist = np.histogram(img[:,:,0], bins=hist_bin, range=hist_range)
 	channel2_hist = np.histogram(img[:,:,1], bins=hist_bin, range=hist_range)
 	channel3_hist = np.histogram(img[:,:,2], bins=hist_bin, range=hist_range)
-	# Concatenate the histograms into a single feature vector
 	hist_features = np.concatenate((channel1_hist[0], channel2_hist[0], channel3_hist[0]))
-	# Return the individual histograms, bin_centers and feature vector
 	return hist_features
 
-# Define a function to return HOG features and visualization
 def __get_hog_features(img, vis=False):
+	""" Function to return HOG features and visualization """
 	if vis == True:
 		features, hog_image = hog(img, orientations=orientations, pixels_per_cell=(pix_per_cell, pix_per_cell),
 			cells_per_block=(cell_per_block, cell_per_block), transform_sqrt=True, 
@@ -85,24 +85,33 @@ def __get_hog_features(img, vis=False):
 		return features
 
 def single_img_features(img):
+	""" Function to extract image features of single image """
 	feature_image = __convert_to_colorspace(img)
 	spatial_features = __bin_spatial(feature_image)
 	hist_features = __color_hist(feature_image)
 	if hog_channel == 'ALL':
 		hog_features = []
-		for channel in range(feature_image.shape[2]):
-			hog_features.append(__get_hog_features(feature_image[:,:,channel],
+		copy_img = cv2.cvtColor(np.copy(feature_image), cv2.COLOR_HLS2BGR)
+		copy_img = cv2.cvtColor(copy_img, cv2.COLOR_BGR2HLS)
+		for channel in range(copy_img.shape[2]):
+			hog_features.append(__get_hog_features(copy_img[:,:,channel],
 				vis=False))
-		hog_features = np.ravel(hog_features)        
+		hog_features = np.ravel(hog_features)
 	else:
-		hog_features = np.ravel(__get_hog_features(feature_image[:,:,hog_channel],
+		copy_img = cv2.cvtColor(np.copy(feature_image), cv2.COLOR_HLS2BGR)
+		copy_img = cv2.cvtColor(copy_img, cv2.COLOR_BGR2HLS)
+		hog_features = np.ravel(__get_hog_features(copy_img[:,:,hog_channel],
 			vis=False))
 	return np.concatenate((spatial_features, hist_features, hog_features))
 
 
 
 def extract_for_training(imgs):
-	# Create a list to append feature vectors to
+	""" Function that receives a list of image paths, load each image and
+		calls single_img_features for each. Saves the features for all images
+		and returns all features together. Is needed for the training of
+		the SVM.
+	"""
 	features = []
 	
 	for image_name in imgs:
